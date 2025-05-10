d212cda895bb35f99b8999418a172912611d63d6
ℰ𝓃𝓏ℴ ℱ𝓊𝓀ℯ <150525657+MilesONerd@users.noreply.github.com>
2025-05-10 12:27:57 -0300

    Add files via upload
    Signed-off-by: ℰ𝓃𝓏ℴ ℱ𝓊𝓀ℯ <150525657+MilesONerd@users.noreply.github.com>

---
+++ assets/functions.js
@@ -0,0 +1,13 @@
+/* <![CDATA[ */
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+/* ]]> */
